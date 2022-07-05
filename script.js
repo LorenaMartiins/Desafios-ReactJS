8aeb64027b28ebb795085a2bd8dc1c21d1967985
Pedro Rossi <73256811+Uns0g@users.noreply.github.com>
2022-07-05 18:06:12 -0300

    Adicionando os scripts

---
+++ script.js
@@ -0,0 +1,35 @@
+const imagem = document.querySelector('#produto-foto img');
+const nome = document.getElementById('produto-nome');
+const preco = document.getElementById('produto-preco');
+
+const procura = document.getElementById('input-procura');
+const erroBox = document.getElementById('erro');
+
+window.onload = () => procura.value = '';
+
+procura.addEventListener('keydown', (ev)=>{
+	if(ev.key == 'Enter'){
+		erroBox.style.display = 'none';
+		procurarProduto(procura.value.toLowerCase());
+	}
+});
+
+function procurarProduto(valor){
+	fetch('data.json')
+		.then(response => response.json())
+		.then(data => {
+			let produtosNomes = [];
+			data.forEach(item => produtosNomes.push(item.nome));
+
+			let posicaoDoProduto = produtosNomes.indexOf(valor); 
+			
+			if(posicaoDoProduto == -1){
+				erroBox.style.display = 'block';
+			}
+			else{
+				imagem.src = data[posicaoDoProduto].url;
+				nome.innerText = data[posicaoDoProduto].nome;
+				preco.innerText = data[posicaoDoProduto].preco;
+			}
+		})
+}
